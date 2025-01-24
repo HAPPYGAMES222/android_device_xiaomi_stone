134182dc099a49138fb0da695b5ced2a27790aa5
HAPPYGAMES222 <115911805+HAPPYGAMES222@users.noreply.github.com>
2025-01-24 21:59:17 +0700

    stone: afterlife Flags

--- afterlife_stone.mk
+++ afterlife_stone.mk
@@ -14,28 +14,25 @@ $(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Inherit some common afterlife stuff.
-AFTERLIFE_VERSION_APPEND_TIME_OF_DAY := true
-AFTERLIFE_BUILD_TYPE := UNOFFICIAL
-AFTERLIFE_MAINTAINER := HAPPYGAMES222
-BUILD_AOSP_CAMERA := false
+# AfterEcho (Dolby)
+TARGET_USE_ECHO := true
+
+# Blur
+TARGET_SUPPORTS_BLUR := true
+
+# FOD
+TARGET_UDFPS_ANIMATIONS := false
+
+# Maintainer
+AFTERLIFE_MAINTANER := HAPPYGAMES222
+
+# Faceunlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_NAME := afterlife_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Blur
-TARGET_SUPPORTS_BLUR := true
-
-# Pixel Stuff
-USE_PIXEL_CHARGING := true
-
-# Quick Tap
-TARGET_SUPPORTS_QUICK_TAP := true
 
 BUILD_FINGERPRINT := POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.8.0.UMPMIXM:user/release-keys
 

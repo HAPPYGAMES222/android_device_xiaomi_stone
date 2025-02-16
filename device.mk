726946f0fe4617353d92e5fba7f8327e1281f9e6
HAPPYGAMES222 <115911805+HAPPYGAMES222@users.noreply.github.com>
2025-02-16 15:59:59 +0700

    stone: switch to lineage-libperfmgr

--- device.mk
+++ device.mk
@@ -441,7 +441,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
